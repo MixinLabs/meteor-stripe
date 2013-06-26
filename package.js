53d6085ce6ed1643e438aeb64f03d3c17f15cce1
Arturas Lebedevas <nsmeta@gmail.com>
2013-06-26 12:41:32 +0100

    load external stripe js script

--- package.js
+++ package.js
@@ -2,3 +2,9 @@ Package.describe({
     summary: "Payments for developers. Stripe makes it easy to start accepting credit cards on the web today."
 });
 
+Package.on_use(function (api) {
+    api.use(['templating'], 'client');
+    
+    // Load stripe.js in the <head>
+    api.add_files('stripe.html', 'client');
+});
